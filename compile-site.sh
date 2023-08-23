97a6040539c8d2da3ebe14b7a6419dacbdf19249
pnwarner <patrick.warner@paradoxresearch.net>
2023-08-22 18:47:14 -0700

    Add messages to compile-site.sh

--- compile-site.sh
+++ compile-site.sh
@@ -11,25 +11,53 @@ publish_user="www-data"
 publish_group="www-data"
 publish_user_group="$publish_user:$publish_group"
 
+echo "Creating temporary directory."
+
 mkdir $path_string
 
 #Copy all repo files to new temp dir
+
+echo "Copy engine."
+
 cp -r $current_path/$project_name-engine/* $path_string/
+
+echo "Copy mod-client-info"
 
 cp -r $current_path/$project_name-mods/$project_name-mod-client-info/* $path_string/
 
+echo "Copy mod-quick-gallery"
+
 cp -r $current_path/$project_name-mods/$project_name-mod-quick-gallery/* $path_string/
+
+echo "Copy site-default"
 
 cp -r $current_path/$project_name-sites/$project_name-site-default/* $path_string/
 
+echo "Copy template-default"
+
 cp -r $current_path/$project_name-templates/$project_name-template-default/* $path_string/
+
+echo "Creating media folders"
 
 mkdir $path_string/$project_name/include/media
 mkdir $path_string/$project_name/include/media/site
 
+echo "Remove unnecessary files"
+
 #delete all README.md in new site
 find $path_string/. -name 'README.md' -delete
 
+echo "Copy website to webroot"
+
 sudo cp -r $path_string/$project_name $publish_root
+
+echo "Changing site ownership"
+
 sudo chown -R $publish_user_group $publish_path
+
+echo "Cleaning up."
+
 rm -r $path_string
+
+echo "Done."
+echo "The default site is set to point to 127.0.0.1/"
