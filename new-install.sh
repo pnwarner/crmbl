8685fc4dcec024eec96110ca07384709e0735229
Patrick Warner <patrick.warner@paradoxresearch.net>
2023-09-06 12:57:21 -0700

    Move new install script to root

--- new-install.sh
+++ new-install.sh
@@ -9,7 +9,6 @@ current_path=$(pwd)
 output_folder="crmbl_site"
 path_string="$current_path/$output_folder"
 project_name="crmbl"
-project_repo="https://github.com/pnwarner/$project_name"
 publish_root="/var/www/html"
 publish_path="$publish_root/$project_name"
 publish_user="www-data"
@@ -19,26 +18,26 @@ publish_user_group="$publish_user:$publish_group"
 #echo "Creating temporary directory."
 mkdir $path_string
 #Copy all repo files to new temp dir
-#echo "Copy engine."
 cp -r $current_path/$project_name-engine/* $path_string/
-#echo "Copy mod-client-info"
 cp -r $current_path/$project_name-mods/$project_name-mod-client-info/* $path_string/
-#echo "Copy mod-quick-gallery"
 cp -r $current_path/$project_name-mods/$project_name-mod-quick-gallery/* $path_string/
-#echo "Copy site-default"
 cp -r $current_path/$project_name-sites/$project_name-site-default/* $path_string/
-#echo "Copy template-default"
 cp -r $current_path/$project_name-templates/$project_name-template-default/* $path_string/
-#echo "Creating media folders"
+#Create media folders
 mkdir $path_string/$project_name/include/media
 mkdir $path_string/$project_name/include/media/site
-#echo "Remove unnecessary files"
 #delete all README.md in new site
 find $path_string/. -name 'README.md' -delete
-#echo "Copy website to webroot"
+#Copy website to webroot path
 sudo cp -r $path_string/$project_name $publish_root
-#echo "Changing site ownership"
+#Changing site ownership
 sudo chown -R $publish_user_group $publish_path
 
+#Move install script to /crmbl-utils and copy compile-site to project root for
+#re-deployment
+mv new-install.sh crmbl-utils/bash/
+cp crmbl-utils/bash/compile-site.sh ./
+rm -rf .git
+
 echo "Done."
-echo "The default site is set to point to http://127.0.0.1:8080/"
+echo "The default site is set to point to http://127.0.0.1/"
