cc570500ba4f6eef8c0f2fb09de593a6869409e5
Patrick Warner <patrick.warner@paradoxresearch.net>
2023-11-05 16:10:17 -0800

    Squashed 'crmbl-templates/' changes from 5c0b788..213261b
    213261b Merge commit '0d8609646c87e5439fe9cee843ed6c4458ab0a9f'
    0d86096 Squashed 'crmbl-template-default/' changes from fd239ab..81f9121
    
    git-subtree-dir: crmbl-templates
    git-subtree-split: 213261b38240a749bf2717c5308ab945d88ee8b3

--- crmbl-template-default/crmbl/template/link/default/link.php
+++ crmbl-template-default/crmbl/template/link/default/link.php
@@ -1,20 +1,47 @@
 <?php
     global $site_config;
     verbose_check("link.php Loaded.");
-    //This default template should load a static main.css, and then load .css files over it
-    //for further customization.
-    //Statically load include/style/template/<Template Name>/
-    // instead of:
-    // include/style/site/<SITENAME>/main.css.php
+    $theme_css = array(
+        'main' => array(
+            'rel' => ["stylesheet"],
+            'type' => 'text/css',
+            'sizes' => '',
+            'target' => '',
+            'name' => 'main.css.php',
+            'path-type' => 'style',
+            'media-path-type' => 'template',
+            'path-name' => 'default',
+            'media-path' => '',
+            'url' => '',
+            'media' => '',
+            'version' => '1.0'
+        )
+    );
 
-    //THEN, the folder of include/style/site/<SITENAME>/*.css *.css.php
-    //All files should be loaded into a list, and included
-
-    //$site_config['theme_link_stylesheet'] should be made irrelevant.  themes should have a 
-    //standard naming. and /include/style/site/<site name>/*.css can be loaded after the fact.
+    foreach ($site_config['link_media_list'] as $key => $value) {
+        insert_link_resource($value);
+    }
+    if ($site_config['theme_link'] != $site_config['theme_body']) {
+        $body_css = array(
+            'rel' => ["stylesheet"],
+            'type' => 'text/css',
+            'sizes' => '',
+            'target' => '',
+            'name' => 'main.css.php',
+            'path-type' => 'style',
+            'media-path-type' => 'template',
+            'path-name' => $site_config['theme_body'],
+            'media-path' => '',
+            'url' => '',
+            'media' => '',
+            'version' => '1.0'
+        );
+        array_push($theme_css, $body_css);
+    }
+    $theme_css = array_merge($theme_css, $site_config['include_styles']);
+    $theme_css = array_merge($theme_css, $site_config['include_additional_styles']);
+    $theme_css = array_merge($theme_css, $site_config['include_mod_styles']);
+    foreach($theme_css as $key => $value) {
+        insert_link_resource($value);
+    }
 ?>
-<link rel="icon" href="/include/media/site/<?=$site_config['site_name']?>/favicon/favicon.ico">
-<link rel="icon" href="/include/media/site/<?=$site_config['site_name']?>/favicon/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/include/media/site/<?=$site_config['site_name']?>/favicon/apple-touch-icon.png">
-<!--Use stylesheet from whichever theme is chosen for Body element.-->
-<link rel="stylesheet" href="/include/style/template/<?=$site_config['theme_body']?>/main.css.php?v=1.0">
